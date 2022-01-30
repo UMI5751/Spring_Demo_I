d8616cdfae7ecf97c0a88107941857a9e6aec106
Bowen Zhang <u7274475@anu.edu.au>
2022-01-30 12:11:20 +1100

    Initial commit

---
+++ src/main/java/com/bowen/zhang/spring_demo_i/SpringDemoIApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.zhang.spring_demo_i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bowen/zhang/spring_demo_i/SpringDemoIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.zhang.spring_demo_i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
